aa2af69f4b73f40818cf5f3d4f0efbbb45d96d40
Célio Gomes <celiogomesdelima@gmail.com>
2024-12-02 15:25:32 -0300

    Adicionado o campo Category aos Models e ao Admin

--- contact/admin.py
+++ contact/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from contact import models
 
 # Register your models here.
+
+@admin.register(models.Category)
+class GategoryAdmin(admin.ModelAdmin):
+    list_display = 'id','name',
+    ordering = 'id',
+    list_filter = 'name',
+    search_fields = 'id', 'name',
+    list_per_page = 10
+    list_max_show_all = 20
+    list_editable = 'name',
+    list_display_links = 'id',
+
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'id','first_name', 'last_name', 'phone',
